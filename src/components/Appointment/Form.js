e008e4db172d1596d3b7acdec342167d7690761b
ramanmand <mandr@sheridancollege.ca>
2021-11-03 17:44:19 -0400

    refactor code to include add create appointments

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,58 +5,48 @@ import { action } from '@storybook/addon-actions/dist/preview';
 
 export default function Form(props) {
 
+  //For keeping track of the name
   const [currentName, setName] = useState(props.name || "");
   const [currentInterviewer, setInterviewer] = useState(props.value || null)
-  const [error, setError] = useState("");
 
+  //Helper function to clear all fields
   const reset = () => {
     setName("")
-    setInterviewer(null)
+    setInterviewer('null')
   }
+
   function cancel () {
+    // props.onCancel;
     reset();
     props.onCancel()
   }
 
-  function validate() {
-    if (currentName === "") {
-      setError("Student name cannot be blank")
-      return;
-    }
-    setError("")
-    props.onSave(currentName, currentInterviewer);
-  }
-
-
-  return(
+  return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        onChange={(event) => setName(event.target.value)}   
-        value={currentName}
-        placeholder="Enter Student Name"
-        data-testid="student-name-input"
-        /*
-          This must be a controlled component
-          your code goes here
-        */
-      />
-      <section className="appointment__validation">{error}</section>
-    </form>
-    <InterviewerList interviewers={props.interviewers} value={currentInterviewer} onChange={(event) => setInterviewer(event)} 
-      /* your code goes here */
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onSubmit={event => event.preventDefault()} onClick={validate}>Save</Button>
-    </section>
-  </section>
-</main>
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={props.name}
+            type="text"        
+            onChange={(event) => setName(event.target.value)}            
+            value={currentName}
+            placeholder={currentName ? currentName : "Please enter your name"}
+
+            /*
+              This must be a controlled component
+            */
+          />
+
+        </form>
+        <InterviewerList interviewers={props.interviewers} value={currentInterviewer} onChange={(event) => setInterviewer(event)} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onSubmit={event => event.preventDefault()} onClick={event => props.onSave(currentName, currentInterviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
   )
 }
