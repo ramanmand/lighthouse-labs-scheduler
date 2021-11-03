3d7560b1f7955f6bfc9c3be7bd8ae45ded672273
ramanmand <mandr@sheridancollege.ca>
2021-11-03 17:14:23 -0400

    update selectors for daylist and show, create new selector file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,56 +1,70 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+
 import "components/Application.scss";
-import DayList from "components/DayList";
+import DayList from "./DayList";
+import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
-import Axios from "axios";
-import 
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
+import axios from 'axios';
+import { matchAppointments, getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
-  const [value, onChange] = useState("Monday");
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
+
+  const setDay = day => setState({...state, day});
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+    // const setDays = days => setState(prev => {
+    //   console.log(prev);
+    //   return ({ ...prev, days })
+    // });
+
+
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get('http://localhost:8001/api/days')),
+      Promise.resolve(axios.get('http://localhost:8001/api/appointments'))
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    })
+  }, [])
+
+  const appointmentObjects = getAppointmentsForDay(state, state.day)
+
+  const appointment = appointmentObjects.map((appointmentObject) => {
+    return (
+        <Appointment key={appointmentObject.id} {...appointmentObject} />
+      )
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  day={"value"}
-  setDay={onChange}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+        <DayList
+          days={state.days}
+          day={state.day}
+          setDay={setDay}
+        />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,15 +3,13 @@ import DayListItem from 'components/DayListItem';
 
 // DayList renders a list of DayListItem components
 export default function DayList(props) {
-  const schedule = props.days.map(dayObj => {  
-  
+  const schedule = props.days.map(dayObj => {
     return (
       <DayListItem
         key={dayObj.id}
-        name={dayObj.name}
-        spots={dayObj.spots}
-        selected={dayObj.name === props.day}
-        setDay={props.setDay} />         
+        selected={dayObj.name === props.day} 
+        {...dayObj}       
+      />
     )
   })
   return <ul>{schedule}</ul>
