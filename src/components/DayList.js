ebee5952c43c85db36b3692dfd3ba7bf775ee9bb
ramanmand <mandr@sheridancollege.ca>
2021-11-03 16:56:52 -0400

    add selectors to other files

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,23 +1,18 @@
-import React from "react";
-import DayListItem from "./DayListItem";
+import React from 'react';
+import DayListItem from 'components/DayListItem';
 
-export default function DayList(props){
-  let firstDaySchedule = "";
-  if (Array.isArray(props.days)) {
-    firstDaySchedule = props.days.map(day => (
+// DayList renders a list of DayListItem components
+export default function DayList(props) {
+  const schedule = props.days.map(dayObj => {  
+  
+    return (
       <DayListItem
-      key={props.id} 
-      name={props.name} 
-      spots={props.spots} 
-      selected={props.name === props.value}
-      setDay={props.onChange}
-     />
-    
-    ))
-  }
-  return (
-    <ul>
-      {firstDaySchedule}
-    </ul>
-  )
+        key={dayObj.id}
+        name={dayObj.name}
+        spots={dayObj.spots}
+        selected={dayObj.name === props.day}
+        setDay={props.setDay} />         
+    )
+  })
+  return <ul>{schedule}</ul>
 }
